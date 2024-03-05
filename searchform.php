ea8a83dea5c1e2b8172f5e91cecc1117d46ce436
Tomoo Suzuki <solosoprani2002@gmail.com>
2024-03-05 09:44:12 +0900

    search form 追加

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form id="form" action="自分のサイトURL" method="get">
+<form role="search" method="get" id="searchform" action="<?php echo home_url('/'); ?>">
     <input id="s-box" name="s" type="text" placeholder="キーワードを入力" />
     <button type="submit" id="s-btn-area">
         <div id="s-btn">検索</div>
