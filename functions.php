318d81e74e0b4532fb7756cc6ecf688f730ed986
Tomoo Suzuki <solosoprani2002@gmail.com>
2025-04-11 22:05:09 +0900

    functions 修正

--- functions.php
+++ functions.php
@@ -8,7 +8,27 @@ register_sidebar(array(
   'after_widget' => '</div>',
   'before_title' => '<h3 class="style_ttl_h3">',
   'after_title' => '</h3>'
-  ));
-register_nav_menus(array(
-  'nav'=>'ナビゲーション'
 ));
+register_nav_menus(array(
+  'nav' => 'ナビゲーション'
+));
+
+function enqueue_scripts_and_styles()
+{
+  wp_deregister_script('jquery');
+  wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Kaisei+Opti&family=Stick&family=Yusei+Magic&display=swap', [], null);
+  wp_enqueue_style('swiper-css', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css', [], null);
+  wp_enqueue_style('custom-style', get_template_directory_uri() . '/webit/scss/style.css', [], null);
+  wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.6.0.js', [], null, true);
+  wp_enqueue_script('swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', [], null, true);
+  wp_enqueue_script('gsap', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js', [], null, true);
+  wp_enqueue_script('gsap-custom-ease', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/CustomEase.min.js', ['gsap'], null, true);
+  wp_enqueue_script('gsap-scroll-trigger', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js', ['gsap'], null, true);
+  wp_enqueue_script('gsap-scroll-to-plugin', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollToPlugin.min.js', ['gsap'], null, true);
+  wp_enqueue_script('custom-script', get_template_directory_uri() . '/js/webit.js"', ['jquery'], null, true);
+  $template_directory_uri = array(
+    'templateUrl' => get_template_directory_uri(),
+  );
+  wp_localize_script('custom-script', 'get_template_directory_uri', $template_directory_uri);
+}
+add_action('wp_enqueue_scripts', 'enqueue_scripts_and_styles');
